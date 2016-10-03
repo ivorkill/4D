6ea100ca76f927ad6efeced07fe68061336b0a27
ivorkill <davevanrossum@gmail.com>
2016-10-03 15:12:58 +0200

    fixed index

--- index.php
+++ index.php
@@ -5,22 +5,28 @@ require_once 'config/dbconnect.php';
 $action = (empty($_GET['action'])) ? '' : $_GET['action'];
 switch ($action) {
 	case 'register_teams':
-		require 'models/add_teams.php';
 		include 'views/register_teams.php';
+		break;
+	case 'add _teams':
+		include 'models/add_teams.php';
 		break;
 	case 'select_teams':
 		require 'models/select_teams.php';
 		break;
 	case 'register_schools':
-		require 'models/add_schools.php';
 		include 'views/register_schools.php';
+		break;
+	case 'add_schools':
+		include 'models/add_schools.php';
 		break;
 	case 'select_schools':
 		require 'models/select_schools.php';
 		break;
 	case 'register_classes':
-		require 'models/add_classes.php';
 		include 'views/register_classes.php';
+		break;
+	case 'add_classes':
+		include 'models/add_classes.php';
 		break;
 	case 'select_classes':
 		require 'models/select_classes.php';
