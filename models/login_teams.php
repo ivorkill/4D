8f251cb5407acfec05484e3e7b8a862f8c68d260
ivorkill <davevanrossum@gmail.com>
2016-11-11 08:40:54 +0100

    working on login error

--- models/login_teams.php
+++ models/login_teams.php
@@ -1,21 +1,21 @@
 <?php
 	$team_name = $mysqli->real_escape_string($_POST['team_name']);
 	$password = hash('sha512', $mysqli->real_escape_string($_POST['password']));
-	$result = $mysqli->query ("SELECT * FROM teams WHERE team_name='$team_name' AND password='$password'");
+	$sql = "SELECT * FROM teams WHERE team_name='$team_name' AND password='$password'";
+	$result = $mysqli->query($sql);
 	$count = $result->num_rows;
+	echo $count;
 	while ($login = $result->fetch_array()) {
-		if($count==1)
+		if($count == 1)
 		{
 			// make the user session equal to the id of the team
 			$_SESSION['user'] = $login['id'];
 			header("Location:?action=account");
 		}
-		else
-		{
-			// echo "Incorrect team name and/or password";
+		elseif ($count == 0) {
+			echo "Incorrect team name and/or password";
 			// echo "<a href='?action=home'>Go back</a>";
-			header("Location:?action=home");
+			// header("Location:?action=home");
 		}
 	}
 ?>
-7
